f6fdafcbc1cfb72ce7844499afb68747e11d0510
xyzstore <veliciaa0932@gmail.com>
2026-05-03 21:13:28 +0800

    Update res1 function to handle menu.zip
    Refactor res1 function to download and extract menu.zip instead of new8.zip. Remove unnecessary commands and clean up temporary files.

--- update.sh
+++ update.sh
@@ -33,22 +33,12 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    rm -rf /tmp/new8-master /tmp/new8.zip
-
-    apt install -y unzip curl dos2unix >/dev/null 2>&1
-
-    curl -L -o /tmp/new8.zip https://github.com/xyzstore/new8/archive/refs/heads/master.zip
-    unzip -o /tmp/new8.zip -d /tmp >/dev/null 2>&1
-
-    if [ ! -d /tmp/new8-master/limit/menu ]; then
-        echo "Folder menu tidak ditemukan di GitHub archive."
-        exit 1
-    fi
-
-    chmod +x /tmp/new8-master/limit/menu/*
-    cp -f /tmp/new8-master/limit/menu/* /usr/local/sbin/
-
-    dos2unix /usr/local/sbin/* 2>/dev/null
+    rm -f menu.zip
+    wget -O menu.zip https://raw.githubusercontent.com/xyzstore/new8/master/limit/menu.zip
+    7z x menu.zip -p'coding_sendiri_lah_goblok_cuman_bisa_nyuri'
+    chmod +x menu/*
+    mv -f menu/* /usr/local/sbin/
+    dos2unix /usr/local/sbin/install-plugin 2>/dev/null
 
     cat >/root/.profile <<'EOF'
 # ~/.profile: executed by Bourne-compatible login shells.
@@ -66,7 +56,7 @@ EOF
 
     chmod 644 /root/.profile
 
-    rm -rf /tmp/new8-master /tmp/new8.zip update.sh
+    rm -rf menu menu.zip update.sh
 }
 netfilter-persistent
 clear
