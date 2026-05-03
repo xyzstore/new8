91898c29cdccce6eb1b7c12f9d3561bcff020480
xyzstore <veliciaa0932@gmail.com>
2026-05-03 21:33:59 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,43 +1,54 @@
 #!/bin/bash
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-red() { echo -e "\\033[32;1m${*}\\033[0m"; }
+
+red() {
+    echo -e "\033[32;1m${*}\033[0m"
+}
+
 clear
-if ! command -v 7z >/dev/null 2>&1; then
-    apt update && apt install p7zip-full -y
-fi
+
 fun_bar() {
     CMD[0]="$1"
     CMD[1]="$2"
+
     (
-        [[ -e $HOME/fim ]] && rm $HOME/fim
-        ${CMD[0]} -y >/dev/null 2>&1
-        ${CMD[1]} -y >/dev/null 2>&1
-        touch $HOME/fim
+        [[ -e $HOME/fim ]] && rm -f "$HOME/fim"
+        ${CMD[0]} >/dev/null 2>&1
+        ${CMD[1]} >/dev/null 2>&1
+        touch "$HOME/fim"
     ) >/dev/null 2>&1 &
+
     tput civis
-    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    echo -ne " \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+
     while true; do
         for ((i = 0; i < 18; i++)); do
             echo -ne "\033[0;32m#"
             sleep 0.1s
         done
-        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+
+        if [[ -e $HOME/fim ]]; then
+            rm -f "$HOME/fim"
+            break
+        fi
+
         echo -e "\033[0;33m]"
         sleep 1s
         tput cuu1
         tput dl1
-        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+        echo -ne " \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
     done
+
     echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
     tput cnorm
 }
+
 res1() {
     clear
     echo -e "Updating menu from GitHub folder..."
 
     rm -rf /tmp/new8-master /tmp/new8.zip
 
+    apt update -y >/dev/null 2>&1
     apt install -y unzip curl dos2unix >/dev/null 2>&1
 
     curl -L -o /tmp/new8.zip https://github.com/xyzstore/new8/archive/refs/heads/master.zip
@@ -78,17 +89,22 @@ EOF
 
     echo -e "Update menu selesai."
 }
-netfilter-persistent
+
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\033[1;96m              UPDATE SCRIPT              \E[0m"
+echo -e "\033[1;96m UPDATE SCRIPT \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e "  \033[1;91m update script service\033[1;37m"
+echo -e " \033[1;91m update script service\033[1;37m"
+
 fun_bar 'res1'
+
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
 read -n 1 -s -r -p "Press [ Enter ] to back on menu"
-menu
 
-###########- COLOR CODE -##############
+if command -v menu >/dev/null 2>&1; then
+    menu
+else
+    /usr/local/sbin/menu
+fi
